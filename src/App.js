9afac7790a4a4883f957f84944ec71a7d4990aed
sonirupal <rupalsoni07@gmail.com>
2024-04-29 13:25:23 +0530

    NavBar Design Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>IGstudio</h1>
+    <div className="container">
+    <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+import vectorImg from '../../assests/Vector.png'
+import igStudioImg from '../../assests/Igstudio.png'
+
+const Navbar = () => {
+  return (
+    <nav className='d-flex justify-content-between align-items-centre mt-5 header'>
+        <div>
+            <img src={vectorImg} alt=''/>
+             <img src={igStudioImg} alt=''/>
+       </div>
+        <div className='d-flex justify-content-evenly' style={{width:'50%'}}>
+            <div>Home</div>
+            <div>Attorneys</div>
+            <div>Practice Areas</div>
+            <div>About Us</div>
+        </div>
+        <div ><button type="button" className="btn btn-outline-light Contactbtn">Contact Now</button></div>
+    </nav>
+  )
+}
+
+export default Navbar
